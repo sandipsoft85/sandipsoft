f334d9bb5e8dbe4bac86f00a3d06deb58b1965c7
sandip mondal <sandip_mondal85@yahoo.co.in>
2013-08-11 01:09:16 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sandipsoft::Application.config.secret_key_base = 'c9a634bafb57d19e38b7c4ff01c94e0afee31d8de4c4a75dfe5c5ae389aac2d415379d9dadb462e306a59d9ced6c5b9a4b1e66045ff420cdc5080cf0bb31b1d6'
